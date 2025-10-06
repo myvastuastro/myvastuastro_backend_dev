6705d8f4a08d206a904f110db50bc0fc3e08d582
Sunder Sharma <sunder.sharma@gllfl.com>
2025-10-06 22:42:26 +0530

    fixed upload file issue

--- dist/models/selfProduct.js
+++ dist/models/selfProduct.js
@@ -5,21 +5,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const selfProductSchema = new mongoose_1.default.Schema({
-    userId: {
-        type: mongoose_1.default.Types.ObjectId,
-        ref: "user",
-        required: false
-    },
-    productName: {
-        type: String,
-        required: false
-    },
-    fileUrl: {
-        type: String,
-        required: false
-    }
+    userId: { type: mongoose_1.default.Types.ObjectId, ref: "user", required: true },
+    productName: { type: String, required: true },
+    fileUrl: { type: String, required: true },
+    originalName: { type: String, required: true },
+    mimeType: { type: String, required: true },
+    resourceType: { type: String, enum: ["image", "raw"], required: true }
 }, {
     timestamps: true
 });
-const product = mongoose_1.default.model("vastuSelfProduct", selfProductSchema);
-exports.default = product;
+const SelfProduct = mongoose_1.default.model("vastuSelfProduct", selfProductSchema);
+exports.default = SelfProduct;
+// import mongoose from "mongoose";
+// const selfProductSchema = new mongoose.Schema({
+//   userId: {
+//     type: mongoose.Types.ObjectId,
+//     ref: "user", 
+//     required: false
+//   },
+//   productName: {
+//     type: String,
+//     required: false
+//   },
+//   fileUrl: {
+//     type: String,
+//     required: false 
+//   }
+// }, {
+//   timestamps: true
+// });
+// const product = mongoose.model("vastuSelfProduct", selfProductSchema);
+// export default product;
--- src/models/selfProduct.ts
+++ src/models/selfProduct.ts
@@ -1,21 +1,36 @@
 import mongoose from "mongoose";
+
 const selfProductSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Types.ObjectId,
-    ref: "user", 
-    required: false
-  },
-  productName: {
-    type: String,
-    required: false
-  },
-  fileUrl: {
-    type: String,
-    required: false 
-  }
+  userId: { type: mongoose.Types.ObjectId, ref: "user", required: true },
+  productName: { type: String, required: true },
+  fileUrl: { type: String, required: true },
+  originalName: { type: String, required: true },
+  mimeType: { type: String, required: true },
+  resourceType: { type: String, enum: ["image", "raw"], required: true }
 }, {
   timestamps: true
 });
 
-const product = mongoose.model("vastuSelfProduct", selfProductSchema);
-export default product;
+const SelfProduct = mongoose.model("vastuSelfProduct", selfProductSchema);
+export default SelfProduct;
+// import mongoose from "mongoose";
+// const selfProductSchema = new mongoose.Schema({
+//   userId: {
+//     type: mongoose.Types.ObjectId,
+//     ref: "user", 
+//     required: false
+//   },
+//   productName: {
+//     type: String,
+//     required: false
+//   },
+//   fileUrl: {
+//     type: String,
+//     required: false 
+//   }
+// }, {
+//   timestamps: true
+// });
+
+// const product = mongoose.model("vastuSelfProduct", selfProductSchema);
+// export default product;
